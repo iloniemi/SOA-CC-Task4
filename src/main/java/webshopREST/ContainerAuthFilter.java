43841beca7cf7e98b02b366affdc04ee51c19d65
iiheiiva <36442009+iiheiiva@users.noreply.github.com>
2020-10-06 01:25:21 +0300

    JWT tarkistus tehty loppuun
    * JWT tokenien tarkistaminen vaikuttaa toimivan oikein sekä adminilla että userilla. Vanhentuneita tokeneita ei hyväksytä.
    * Jostain syystä tyhjää tilaa sisältävä token päästi autorisoinnin läpi, fiksasin nopealla tarkistuksella mutta pitäisi varmaan katsoa tarkemmin mistä johtui.
    * Väärin formatoitujen tokenien lähettäminen voi johtaa Internal server erroreihin. JWT-autorisointi-koodin  voisi ympäröidä try-catcheillä tämän välttämiseksi

--- src/main/java/webshopREST/ContainerAuthFilter.java
+++ src/main/java/webshopREST/ContainerAuthFilter.java
@@ -1,8 +1,12 @@
 package webshopREST;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Method;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -14,9 +18,16 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
+import org.glassfish.jersey.server.internal.process.MappableException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import types.ErrorMessage;
 import types.User;
 import webshopREST.database.SingletonDatabase;
+import webshopREST.database.TokenService;
 import webshopREST.errors.DataNotFoundException;
 import webshopREST.errors.InvalidInputException;
 
@@ -24,6 +35,7 @@ import javax.annotation.Priority;
 import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.json.JsonObject;
 
 @Provider
 @Priority(Priorities.AUTHENTICATION)
@@ -32,13 +44,15 @@ public class ContainerAuthFilter implements ContainerRequestFilter {
 	private static final String AUTHORIZATION_HEADER_PREFIX_BASIC = "Basic ";
 	private static final String AUTHORIZATION_HEADER_PREFIX_JWT = "Bearer ";
 	private final SingletonDatabase database = SingletonDatabase.getDatabase();
-	
-	private static final ErrorMessage FORBIDDEN_ErrMESSAGE = new ErrorMessage("Access blockedfor all users !!!", 403, "http://myDocs.org");
-	private static final ErrorMessage UNAUTHORIZED_ErrMESSAGE = new ErrorMessage("User cannot access the resource.", 401, "http://myDocs.org");
-	
-	
-	@Context private ResourceInfo resourceInfo;
-	
+
+	private static final ErrorMessage FORBIDDEN_ErrMESSAGE = new ErrorMessage("Access blockedfor all users !!!", 403,
+			"http://myDocs.org");
+	private static final ErrorMessage UNAUTHORIZED_ErrMESSAGE = new ErrorMessage("User cannot access the resource.",
+			401, "http://myDocs.org");
+
+	@Context
+	private ResourceInfo resourceInfo;
+
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 		System.out.println("filter-metodissa");
@@ -48,10 +62,10 @@ public class ContainerAuthFilter implements ContainerRequestFilter {
 		String authToken = null;
 		if (authHeader != null && authHeader.size() > 0) {
 			authToken = authHeader.get(0);
-		}	
-		
-		//Basic authentication
-		if (authToken != null && authToken.startsWith(AUTHORIZATION_HEADER_PREFIX_BASIC)) {	
+		}
+
+		// Basic authentication
+		if (authToken != null && authToken.startsWith(AUTHORIZATION_HEADER_PREFIX_BASIC)) {
 			authToken = authToken.replaceFirst(AUTHORIZATION_HEADER_PREFIX_BASIC, "");
 			String decodedString = new String(Base64.getDecoder().decode(authToken));
 			System.out.println("decoded: " + decodedString);
@@ -59,7 +73,7 @@ public class ContainerAuthFilter implements ContainerRequestFilter {
 				throw new InvalidInputException("Login information missing");
 			}
 			StringTokenizer tokenizer = new StringTokenizer(decodedString, ":");
-			//if (tokenizer.toString().contains(" : "))
+			// if (tokenizer.toString().contains(" : "))
 			String username = tokenizer.nextToken();
 			String password = tokenizer.nextToken();
 			if (database.userCredentialExists(username, password)) {
@@ -71,61 +85,115 @@ public class ContainerAuthFilter implements ContainerRequestFilter {
 			}
 		}
 		
-		//JWT authentication
-		if (authToken != null && authToken.startsWith(AUTHORIZATION_HEADER_PREFIX_JWT)) {	
+		//TODO: tyhjät tokenit pääsevät autorisoinnin läpi, tämä on nopea fixi
+		System.out.println(authToken);
+		if (authToken.trim().equals("Bearer")) throw new InvalidInputException ("Empty tokens not allowed");
+		System.out.println(authToken);
+		
+		
+		// JWT authentication
+		if (authToken != null && authToken.startsWith(AUTHORIZATION_HEADER_PREFIX_JWT)) {
+			System.out.println("Löytyi auth token: " + authToken);
+
 			authToken = authToken.replaceFirst(AUTHORIZATION_HEADER_PREFIX_JWT, "");
-			//TODO:tokenin dekoodaus ja vahvistus, userin ja securitycontextin asettaminen.
+
+			String[] parts = authToken.split("\\.");
+
+			// TODO: Wrongly formatted token can cause Internal exception
+			String headers = (new String(Base64.getUrlDecoder().decode(parts[0])));
+			ObjectNode node = new ObjectMapper().readValue(headers, ObjectNode.class);
+
+			if (!(node.get("alg").asText().equals("HS256") && node.get("typ").asText().equals("JWT"))) {
+				throw new InvalidInputException("Given token is not of a supported type (JWT with HS256 hashing)");
+
+			}
+			String payload = (new String(Base64.getUrlDecoder().decode(parts[1])));
+			String signature = (new String(Base64.getUrlDecoder().decode(parts[2])));
+
+			// Check the expiration
+			node = new ObjectMapper().readValue(payload, ObjectNode.class);
+			Long now = new Date().getTime();
+			Long expiration = node.get("exp").asLong();
+			if (expiration < now) {
+				throw new InvalidInputException("The given token has expired " + ((now - expiration) / 1000)
+						+ " seconds ago. Request a new token.");
+			}
+			
+			//Check if the token has been signed by the service
+			Boolean authorized = false;
+			try {
+				authorized = TokenService.checkTokenValidity(payload, signature);
+			} catch (InvalidKeyException | JsonProcessingException | UnsupportedEncodingException
+					| NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			if (!authorized) {
+				throw new InvalidInputException("Given token has not been authorized by the service");
+			}
+			
+			//Autorisointi
+			user = database.getUser(node.get("sub").asText());
+			System.out.println("filter loop, user löydetty " + user.getName());
+			String scheme = requestContext.getUriInfo().getRequestUri().getScheme();
+			requestContext.setSecurityContext(new WebshopSecurityContext(user, scheme));
+			System.out.println("");
+
 		}
-		
-		
-		//Authorization
-		//Requested method annotations are checked first
+
+		// Authorization
+		// Requested method annotations are checked first
 		Method resMethod = resourceInfo.getResourceMethod();
-		
-		if(resMethod.isAnnotationPresent(PermitAll.class)) return;
-		
-		if(resMethod.isAnnotationPresent(DenyAll.class)){
-		    Response response = Response.status(Response.Status.FORBIDDEN).entity(FORBIDDEN_ErrMESSAGE).build();
-		    requestContext.abortWith(response);
+
+		if (resMethod.isAnnotationPresent(PermitAll.class))
+			return;
+
+		if (resMethod.isAnnotationPresent(DenyAll.class)) {
+			Response response = Response.status(Response.Status.FORBIDDEN).entity(FORBIDDEN_ErrMESSAGE).build();
+			requestContext.abortWith(response);
 		}
 
-		if(resMethod.isAnnotationPresent(RolesAllowed.class)){
-			//If user has role that is allowed in method annotation, filter lets request pass
-		    if(rolesMatched(user,resMethod.getAnnotation(RolesAllowed.class))) return;
-		    
-		    Response response = Response.status(Response.Status.UNAUTHORIZED).entity(UNAUTHORIZED_ErrMESSAGE).build();
-		    requestContext.abortWith(response);
+		if (resMethod.isAnnotationPresent(RolesAllowed.class)) {
+			// If user has role that is allowed in method annotation, filter lets request
+			// pass
+			if (rolesMatched(user, resMethod.getAnnotation(RolesAllowed.class)))
+				return;
+
+			Response response = Response.status(Response.Status.UNAUTHORIZED).entity(UNAUTHORIZED_ErrMESSAGE).build();
+			requestContext.abortWith(response);
 		}
-		
-		//Annotations for the requested resource's class are checked next
+
+		// Annotations for the requested resource's class are checked next
 		Class<?> resClass = resourceInfo.getResourceClass();
-		if(resClass.isAnnotationPresent(DenyAll.class)){
-		    Response response = Response.status(Response.Status.FORBIDDEN).entity(FORBIDDEN_ErrMESSAGE).build();
-		    requestContext.abortWith(response);
+		if (resClass.isAnnotationPresent(DenyAll.class)) {
+			Response response = Response.status(Response.Status.FORBIDDEN).entity(FORBIDDEN_ErrMESSAGE).build();
+			requestContext.abortWith(response);
 		}
 
-		if(resClass.isAnnotationPresent(RolesAllowed.class)){
-		    if(rolesMatched(user,resClass.getAnnotation(RolesAllowed.class))) return;
-		    
-		    Response response = Response.status(Response.Status.UNAUTHORIZED).entity(UNAUTHORIZED_ErrMESSAGE).build();
-		    requestContext.abortWith(response);
+		if (resClass.isAnnotationPresent(RolesAllowed.class)) {
+			if (rolesMatched(user, resClass.getAnnotation(RolesAllowed.class)))
+				return;
+
+			Response response = Response.status(Response.Status.UNAUTHORIZED).entity(UNAUTHORIZED_ErrMESSAGE).build();
+			requestContext.abortWith(response);
 		}
-		
+
 	}
-	
-	
-	/** Checks if given user has a role listed in given RolesAllowed annotation.
+
+	/**
+	 * Checks if given user has a role listed in given RolesAllowed annotation.
 	 * Returns false if user is null.
-	 * @param user whose roles will be checked
+	 * 
+	 * @param user       whose roles will be checked
 	 * @param annotation that contains roles
 	 * @return did user have a role contained in allowed roles
 	 */
 	private boolean rolesMatched(User user, RolesAllowed annotation) {
-		if (user == null) return false;
-		
-		//Ruma, mutta halusin kayttaa varmuuden vuoksi IgnoreCasea.
-		for (String role: annotation.value()) {
-			for (String usersRole: user.getRoles()) {
+		if (user == null)
+			return false;
+
+		// Ruma, mutta halusin kayttaa varmuuden vuoksi IgnoreCasea.
+		for (String role : annotation.value()) {
+			for (String usersRole : user.getRoles()) {
 				if (usersRole.equalsIgnoreCase(role)) {
 					return true;
 				}
@@ -133,7 +201,5 @@ public class ContainerAuthFilter implements ContainerRequestFilter {
 		}
 		return false;
 	}
-				
-
 
 }
--- src/main/java/webshopREST/database/TokenService.java
+++ src/main/java/webshopREST/database/TokenService.java
@@ -1,5 +1,7 @@
 package webshopREST.database;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -8,6 +10,7 @@ import types.AuthenticationToken;
 import types.User;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -20,17 +23,22 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class TokenService {
 	private static String SECRET = "testi";
-	
-	/** Creates token for given user
+
+	/**
+	 * Creates token for given user
+	 * 
 	 * @param user for whom token is created
 	 * @return created token
 	 * @throws UnsupportedEncodingException if something goes wrong with encoding.
-	 * @throws NoSuchAlgorithmException if there's something wrong with signing algorithm
-	 * @throws InvalidKeyException  if there's something wrong with secret key used for signing
+	 * @throws NoSuchAlgorithmException     if there's something wrong with signing
+	 *                                      algorithm
+	 * @throws InvalidKeyException          if there's something wrong with secret
+	 *                                      key used for signing
 	 */
-	public static AuthenticationToken getToken(User user) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException {
-		//TODO: Tokenin luominen.
-		//Header
+	public static AuthenticationToken getToken(User user)
+			throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException {
+		// TODO: Tokenin luominen.
+		// Header
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode headerNode = mapper.createObjectNode();
 		headerNode.put("alg", "HS256");
@@ -38,13 +46,13 @@ public class TokenService {
 		String header = headerNode.toString();
 		String headerB64 = encodeToB64(header);
 		System.out.println("header: " + header);
-		
-		//Payload
+
+		// Payload
 		Date nyt = new Date();
-		long kesto = 3600000; //1 hour in ms
-		
+		long kesto = 3600000; // 1 hour in ms
+
 		ArrayNode rolesNode = mapper.createArrayNode();
-		for (String role: user.getRoles()) {
+		for (String role : user.getRoles()) {
 			rolesNode.add(role);
 		}
 		ObjectNode payloadNode = mapper.createObjectNode();
@@ -54,45 +62,97 @@ public class TokenService {
 		payloadNode.put("exp", nyt.getTime() + kesto);
 		String payload = payloadNode.toString();
 		String payloadB64 = encodeToB64(payload);
-		System.out.println("payload: " + payload);		
-		
+		System.out.println("payload: " + payload);
+
 		// Signature
 		String toBesigned = headerB64 + "." + payloadB64;
-		//String signature = DigestUtils.sha256Hex(toBesigned);
-		//String signature = new String(DigestUtils.sha256(toBesigned.getBytes()));
-		String signature = createSignature(toBesigned);
+		// String signature = DigestUtils.sha256Hex(toBesigned);
+		// String signature = new String(DigestUtils.sha256(toBesigned.getBytes()));
+		String signature = createSignature(toBesigned, SECRET);
 		System.out.println("toBeSigned: " + toBesigned);
 		System.out.println("signature: " + signature);
-		
-		//Result
+
+		// Result
 		AuthenticationToken token = new AuthenticationToken();
 		token.setToken(headerB64 + "." + payloadB64 + "." + signature);
 		return token;
 	}
-	
-	/** Encodes given string to Base64Url string
+
+	/**
+	 * Encodes given string to Base64Url string
+	 * 
 	 * @param mjono String to be encoded
 	 * @return encoded string as string
 	 * @throws UnsupportedEncodingException
 	 */
 	private static String encodeToB64(String mjono) throws UnsupportedEncodingException {
-		return Base64.getEncoder().encodeToString(mjono.getBytes());
+		return Base64.getUrlEncoder().encodeToString(mjono.getBytes());
 	}
 	
 	/**
-	 * @param mjono String to be signed
-	 * @return Signature for the given string
-	 * @throws NoSuchAlgorithmException if algorithm is wrong
-	 * @throws InvalidKeyException if there's something wrong with the key
+	 * Decodes given string from Base64Url string
+	 * 
+	 * @param mjono String to be decoded
+	 * @return decoded String
 	 */
-	private static String createSignature(String mjono) throws NoSuchAlgorithmException, InvalidKeyException {
+	private static String decodeFromB64(String mjono) {
+		return new String(Base64.getUrlDecoder().decode(mjono));
+	}
+	
+	
+	private static String createSignature(String mjono, String secret) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException {
+		return encodeToB64(HmacSHA256(mjono, secret));
+	}
+
+	/**
+	 * @param mjono String to be hashed
+	 * @return HmacSHA256-hash of the given string
+	 * @throws NoSuchAlgorithmException if algorithm is wrong
+	 * @throws InvalidKeyException      if there's something wrong with the key
+	 * @throws UnsupportedEncodingException   if the encoding to B64 fails
+	 */
+	private static String HmacSHA256(String mjono, String secret) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
 		byte[] result = null;
 		String algorithm = "HmacSHA256";
 		Mac mac = Mac.getInstance(algorithm);
-		SecretKeySpec secretKeySpec = new SecretKeySpec(SECRET.getBytes(), algorithm);
+		SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getBytes(), algorithm);
 		mac.init(secretKeySpec);
 		result = mac.doFinal(mjono.getBytes());
-		
-		return new String(result);
+
+		return  new String(result);
 	}
+	
+	
+	/**
+	 * Tries to create  JWT-token with the known secret key and checks if the token matches the sent message
+	 * 
+	 * @param message JWT-token to check
+	 * @return is the token valid
+	 * @throws JsonProcessingException 
+	 * @throws JsonMappingException 
+	 * @throws UnsupportedEncodingException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeyException 
+	 */
+	public static Boolean checkTokenValidity(String payload, String signature) throws JsonMappingException, JsonProcessingException, UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode headerNode = mapper.createObjectNode();
+		headerNode.put("alg", "HS256");
+		headerNode.put("typ", "JWT");
+		String header = headerNode.toString();
+
+		String comparableSignature = createSignature(encodeToB64(header) + "." + encodeToB64(payload), SECRET);
+		signature = encodeToB64(signature);
+		
+		Boolean result = signature.equals(comparableSignature);
+		
+		System.out.println("Jos allekirjoitukset ovat samat, token on autorisoitu: " + result);
+		System.out.println(signature);
+		System.out.println(comparableSignature);
+				
+		return result;
+	}
+	
+
 }
